59f1ad1e0f8f3dfc3296e1a4924a415abe6414b7
Chris Richards <chris.richards@yellowfeather.co.uk>
2020-06-19 21:45:58 +0100

    Initial Commit

---
+++ sixmusic/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  sixmusic
+//
+//  Created by Chris Richards on 19/06/2020.
+//  Copyright © 2020 Yellow Feather Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
